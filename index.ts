7549a903fdd370353cf12e5a6d46ad2aedd8d1cc
Manahil AFAQ <manahilafaq281@gmail.com>
2024-04-06 07:05:05 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,67 @@
+#! /usr/bin/env node
+
+import  inquirer from "inquirer";
+
+let myBalance = 50000;
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            message: "enter your pin",
+            type: "number"
+        }
+    ]
+);
+
+if (pinAnswer.pin === myPin) {
+    console.log("correct pin code!!!");
+
+   let operationAns = await inquirer.prompt(
+        [
+            {
+                name: "operation",
+                message: "please select option",
+                type: "list",
+                choices: ["withdraw","check balance"]
+
+            }
+        ]
+    );
+
+    console.log(operationAns);
+
+    if (operationAns.operation === "withdraw"){
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "enter your amount",
+                    type: "number",
+                }
+            ]
+        );
+
+        if(amountAns.amount <= myBalance && amountAns.amount>=500){
+        // =,-=,+=
+        myBalance -= amountAns.amount
+        console.log("your remaining balace is: " + myBalance)
+    }
+    else{console.log("invalid amount")
+
+    }
+    }
+    
+    else if (operationAns.operation === "check balance"){
+        console.log("your balance is: " + myBalance )
+    }
+}
+
+
+else {
+    console.log("incorrect pin number");
+}
+
+
+
